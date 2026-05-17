fd88b2472f5368bf03489b851f97afc47026de92
akhils <akhilb4001@gmail.com>
2026-05-18 00:39:25 +0530

    learing go basic

---
+++ go.mod
@@ -0,0 +1,3 @@
+module basic_learning
+
+go 1.26.3
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+func main(){
+	fmt.Println("Hello, world!");
+	if true{
+		fmt.Println("this is true");
+	} else {
+		fmt.Println("this is false");
+	}
+	for i:=0;i<5;i++{
+		fmt.Println(i);
+	}
+}
